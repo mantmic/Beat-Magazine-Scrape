421109337908c62b27f910c6e194d4ce1a429c68
mantmic <michael.f.mantfeld@gmail.com>
2018-07-04 12:21:37 +1000

    Placed python scripts into the scrape directory, fixed gigguidescrape bug where every gig was assigned the first gig's genre

--- README.md
+++ README.md
@@ -1,30 +1,33 @@
-# Beat-Magazine-Scrape
+# Beat Magazine Scrape
 
 ## Overview ##
 Beat Magazine has a great gig guide, serving as a repository of local gigs in Melbourne. The user experience of finding a gig on their website however leaves alot to be desired.
 
 http://www.beat.com.au/gig-guide
 
-The objective of these functions is to;
-1. Scrape the gig guide to find events in Melbourne
-2. Associate webpages containing artist music with Beat's artist pages
 
 The end goal is to improve the user experience of finding a gig by;
 1. Giving the user more search options (geospatial, price, etc)
 2. Allowing a user to listen to an artist on the same screen as where they are searching for a gig
 
+### Scrape ###
+The objective of these functions is to;
+1. Scrape the gig guide to find events in Melbourne
+2. Associate webpages containing artist music with Beat's artist pages
+
 These python functions pull the data required to achieve this.
 
-## Dependencies ##
+#### Dependencies ####
+* Python 3
 
-Python packages
-* urllib
-* bs4
-* re (usually installed by default)
-* fuzzywuzzy
-* dateutil
-* importlib (for importing functions from each file)
-* geopy
+* Python 3 libraries
+  * urllib
+  * bs4
+  * re (usually installed by default)
+  * fuzzywuzzy
+  * dateutil
+  * importlib (for importing functions from each file)
+  * geopy
 
 ```bash
 pip3 install urllib bs4 fuzzywuzzy dateutil importlib geopy
---
+++ scrape/beatScrape.py
@@ -0,0 +1,121 @@
+from urllib import request
+from bs4 import BeautifulSoup
+from dateutil import parser
+
+def BeatHeadlineArtistScrape(headlineArtistLink):
+    #scrape headlinining artists
+    page = request.urlopen(headlineArtistLink)
+    soup = BeautifulSoup(page, "html5lib")
+    all_links = soup.find_all("a")
+    try:
+        artistName = [x for x in all_links if x.get("href") != None and x.get("href") in headlineArtistLink and not x.get("href") in ["/"] and x.text != None][0].text
+    except:
+        artistName = None
+        print("Could not parse artist name")
+    return({
+        "artistName":artistName
+    })
+
+
+def BeatGigScrape(gigUrl):
+    page = request.urlopen(gigUrl)
+    soup = BeautifulSoup(page, "html5lib")
+    gig_info = soup.find('div',{"class":"w-clearfix gigguide_gigdetail-details"})
+    gig_details = gig_info.find_all('h5',{"class":"gigguide_node-summary-detail"})
+
+    gigHeadlineArtist = []
+    gigSupportArtist = []
+    gigVenue = None
+    gigPrice = None
+    gigDatetime = None
+
+    #determine what each thing is
+    for i in range(len(gig_details)):
+        gig_detail = gig_details[i]
+        #classify detail
+        gig_link = gig_detail.find("a")
+        gig_span = gig_detail.find("span")
+        if gig_link is None:
+            #not a venue or artist
+            #check if there is a date
+            if gig_span is None:
+                if "$" in gig_detail.text:
+                    #turn into numeric
+                    try:
+                        gigPrice = gig_detail.text
+                        gigPrice=float(''.join(i for i in gigPrice if (i.isdigit() or i in ["."])))
+                    except:
+                        print("Cannot parse gig price (" + gig_detail.text + ")")
+            else:
+                #this the date
+                try:
+                    gigDatetime = gig_span.text
+                    #remove the @ symbol
+                    gigDatetime=''.join(i for i in gigDatetime if not i in ["@"])
+                    #parse as time
+                    #this will be converted to the correct timezone later when the venue location is considered
+                    gigDatetime=parser.parse(gigDatetime).strftime("%Y-%m-%dT%H:%M:%S")
+                except:
+                    print("Cannot parse gig datetime (" + gig_span.text + ")")
+        else:
+            href = gig_link.get("href")
+            if "artist/" in href:
+                gigHeadlineArtist.append(href)
+            elif "support" in href:
+                gigSupportArtist.append(href)
+            elif "venue" in href:
+                gigVenue = href
+    return({
+        "gigHeadlineArtist":gigHeadlineArtist,
+        "gigSupportArtist":gigSupportArtist,
+        "gigVenue":gigVenue,
+        "gigPrice":gigPrice,
+        "gigDatetime":gigDatetime
+    })
+
+#scrape location information
+def BeatVenueScrape(venueUrl):
+    venueName = None
+    venueAddress = None
+    page = request.urlopen(venueUrl)
+    soup = BeautifulSoup(page, "html5lib")
+
+    location_detail=soup.find('div',{"class":"w-clearfix gigguide_gigdetail-details"})
+
+    #get name
+    venueName = soup.find('h1',{"class":"article_title"}).text
+
+    field_labels = location_detail.find_all('h5',{"class":"gigguide_gigdetail-field-label"})
+
+    for i in range(len(field_labels)):
+        	if 'address' in field_labels[i].text.lower():
+        		venueAddress = field_labels[i].findNext('h5').text
+        		if venueAddress != None:
+        			break
+    return({
+        "venueName":venueName,
+        "venueAddress":venueAddress
+    })
+
+def BeatGigGuideScrape(gigGuideUrl):
+    page = request.urlopen(gigGuideUrl)
+    soup = BeautifulSoup(page, "html5lib")
+
+    gigs = soup.find_all("h3", {"class":"archive_node-summary-title"})
+
+    gigGuideGigs = []
+
+    for i in range(len(gigs)):
+        gig = gigs[i]
+        gigName=gig.text
+        gigLink=gig.find("a").get("href")
+        gigGenre=gig.parent.parent.parent.parent.find("a").text
+        #remove the bracket and
+        gigGenre=''.join(i for i in gigGenre if not (i.isdigit() or i in ["(",")"])).strip()
+        #remove the brackets and number
+        gigGuideGigs.append({
+            "gigName":gigName,
+            "gigLink":gigLink,
+            "gigGenre":gigGenre
+        })
+    return(gigGuideGigs)
